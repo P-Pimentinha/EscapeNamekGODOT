87ff0d141bdd4592b2195ac6f6ddc6bf6a3e4fd1
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2024-09-23 22:43:05 +0200

    more changes to UI

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -5,14 +5,23 @@ extends CanvasLayer
 @onready var win_label = $WinLabel
 @onready var background_texture = $background_texture
 @onready var main_menu = $main_menu
-@onready var optnions_menu = $OptionsMenu
+@onready var options_menu = $OptionsMenu
+@onready var tip_menu_main = $TipMenuMain
+@onready var tips_menu_lv_l_1 = $TipsMenuLvL1
+@onready var tips_menu_lv_l_2 = $TipsMenuLvL2
+@onready var tips_menu_lv_l_3 = $TipsMenuLvL3
+@onready var return_init_menu_label = $return_init_menu_label
+@onready var array_of_sub_menus: Array = [main_menu,options_menu, tip_menu_main, tips_menu_lv_l_1, tips_menu_lv_l_2, tips_menu_lv_l_3]
 
 signal unpause_game_hud
 signal start_game_hud
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	visible = true
+	show()
+	main_menu.show()
+	return_init_menu_label.hide()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -24,30 +33,50 @@ func show_victory_label():
 	
 #menu
 func hide_hud():
-	main_menu.visible = false
-	optnions_menu.hide()
-	background_texture.hide()
+	visible = false
+	#main_menu.visible = false
+	#optnions_menu.hide()
+	#background_texture.hide()
 
 func show_hud():
-	main_menu.visible = true
-	#optnions_menu.show()
-	background_texture.show()
+	visible = true
+	#main_menu.visible = true
+	##optnions_menu.show()
+	#background_texture.show()
 
-func show_key_bindings():
+func show_options_menu():
 	main_menu.hide()
-	optnions_menu.show()
+	options_menu.show()
+	return_init_menu_label.show()
 
 func return_initial_menu():
+	for element in array_of_sub_menus:
+		element.hide()
 	main_menu.show()
-	optnions_menu.hide()
+	return_init_menu_label.hide()
+
 
 func menu_controls():
 	if Input.is_action_just_pressed("right") and GameControl.is_new_game:
 		start_game_hud.emit()
+		return_initial_menu()
 	if Input.is_action_just_pressed("right") and !GameControl.is_new_game:
 		unpause_game_hud.emit()
-	if GameControl.is_player_on_menu and Input.is_action_just_pressed("Menu_Show_Keys"):
-		show_key_bindings()
-	if GameControl.is_player_on_menu and Input.is_action_just_pressed("menu_return"):
 		return_initial_menu()
+	if Input.is_action_just_pressed("menu_return"):
+		return_initial_menu()
+		
+	#if GameControl.is_player_on_menu and Input.is_action_just_pressed("menu_return"):
+		#print("return")
+		#return_initial_menu()
+
+func _on_options_button_pressed():
+	show_options_menu()
+
+func _on_tips_button_pressed():
+	main_menu.hide()
+	tip_menu_main.show()
+	return_init_menu_label.show()
+
+
 
