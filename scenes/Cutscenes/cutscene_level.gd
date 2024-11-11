6ea5a5078391a573c4e0b1eaa8e1da06a8f079ea
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2024-11-11 22:19:21 +0100

    adds cutscene_player

--- scenes/Cutscenes/cutscene_level.gd
+++ scenes/Cutscenes/cutscene_level.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
+@onready var level_2_animated_sprite: AnimatedSprite2D = $Level2/Level2_animatedSprite
+@onready var body_1 = $Level2/Body1
 
-#Create 3 animations one after lvl1 one after lvl 2 and the final one ... play accordingly with the level
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,6 +17,11 @@ func _on_timer_timeout():
 	if GameLevelController.current_level == 2:
 		get_tree().change_scene_to_file("res://scenes/Level/level_2.tscn")
 	elif GameLevelController.current_level == 3:
-		get_tree().change_scene_to_file("res://scenes/Level/level_3.tscn") 
+		get_tree().change_scene_to_file("res://scenes/Level/level_3.tscn")
 	elif GameLevelController.current_level == 4:
 		pass
+
+
+func play_animation(animation_to_play: String):
+	level_2_animated_sprite.stop()
+	level_2_animated_sprite.play(animation_to_play)
--- test/del.gd
+++ test/del.gd
@@ -1,16 +1,29 @@
 extends Node2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 
-#@export var frequency = 5
-@export_range(0, 1000) var frequency: float = 10
-@export_range(0, 1000) var amplitude: float = 10
-var time = 0
+##@export var frequency = 5
+#@export_range(0, 1000) var frequency: float = 10
+#@export_range(0, 1000) var amplitude: float = 10
+#var time = 0
+#
+#func _physics_process(delta):
+	#time += delta
+	#var movement = cos(time*frequency) * amplitude
+	#print(movement)
+	#position.y += movement * delta
+	#position.x += 1
 
-func _physics_process(delta):
-	time += delta
-	var movement = cos(time*frequency) * amplitude
-	print(movement)
-	position.y += movement * delta
-	position.x += 1
-	
+func _ready():
+	Engine.max_fps = 60
+	animation_player.play("CutSceneTest")	
+
+func animation_one():
+	animated_sprite_2d.play("first")
+
+func animation_two(number: int):
+	animated_sprite_2d.play("second")
 
 
+func stop_all_animations():
+	animated_sprite_2d.stop()
