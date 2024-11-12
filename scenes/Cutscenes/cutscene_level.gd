4e5fe4aa6eccb2b3331b9a8b3ed726dfaf248834
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2024-11-12 22:51:38 +0100

    completes level2 cutscene

--- scenes/Cutscenes/cutscene_level.gd
+++ scenes/Cutscenes/cutscene_level.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 @onready var level_2_animated_sprite: AnimatedSprite2D = $Level2/Level2_animatedSprite
 @onready var body_1 = $Level2/Body1
+@onready var animation_player = $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	if GameLevelController.current_level == 2:
+		animation_player.play("Level_2_Intro")
+	else:
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,3 +29,6 @@ func _on_timer_timeout():
 func play_animation(animation_to_play: String):
 	level_2_animated_sprite.stop()
 	level_2_animated_sprite.play(animation_to_play)
+
+func advance_to_level2():
+	get_tree().change_scene_to_file("res://scenes/Level/level_2.tscn")
